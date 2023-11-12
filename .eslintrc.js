1cba72c9db34afaf6cbf67e3d441d55b4563844a
Olivier Tassinari <olivier.tassinari@gmail.com>
2023-11-13 04:46:37 +0530

    Import TopBar and Drawer from templates * ref: https://github.com/mui/material-ui/tree/v5.14.17/docs/data/material/getting-started/templates * TODO: 	* Cleanup unnecessary code 	* Add Auth

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,8 @@ module.exports = {
   },
   rules: {
     'react/react-in-jsx-scope': 'off',
+    'react/prop-types': ['off'],
+    'react/jsx-props-no-spreading': ['off'],
     'react/jsx-filename-extension': [1, { extensions: ['.js'] }]
   }
 };
--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Outlet, Navigate } from 'react-
 import Home from './components/Home';
 import PrivateComponent from './components/PrivateComponent';
 import NotFound from './components/NotFound';
+import TopBar from './components/TopBar';
+import Login from './components/Login';
 
 function PrivateRoute() {
   const isLoggedIn = false; // useAuth();
@@ -14,15 +16,17 @@ function PrivateRoute() {
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/login" element={<Login />} /> */}
-        <Route path="private" element={<PrivateRoute />}>
-          <Route path="/private" element={<PrivateComponent />} />
-        </Route>
-        <Route path="/404" element={<NotFound />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <TopBar>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="private" element={<PrivateRoute />}>
+            <Route path="/private" element={<PrivateComponent />} />
+          </Route>
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </TopBar>
     </Router>
   );
 }
