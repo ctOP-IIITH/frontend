24faaf303b1e547f50fd924acccae14735e7a264
vjspranav <pranavasri@live.in>
2023-11-19 03:04:18 +0530

    Add test for authInstance of axios

--- src/components/PrivateComponent.js
+++ src/components/PrivateComponent.js
@@ -1,8 +1,37 @@
+import { Box, Button } from '@mui/material';
+import { axiosAuthInstance } from '../services/axiosConfig';
+
 function PrivateComponent() {
   return (
-    <div>
+    <Box>
       <h1>PrivateComponent</h1>
-    </div>
+      <div
+        style={{
+          display: 'flex',
+          // flexDirection: 'column',
+          gap: '10px'
+        }}>
+        <Button
+          onClick={async () => {
+            const res = await axiosAuthInstance.get('/user/am-i-admin');
+            alert(res.data);
+          }}
+          variant="contained"
+          color="primary">
+          AM I ADMIN
+        </Button>
+        {/* get all users */}
+        <Button
+          onClick={async () => {
+            const res = await axiosAuthInstance.get('/user/getusers');
+            alert(JSON.stringify(res.data));
+          }}
+          variant="contained"
+          color="primary">
+          GET ALL USERS
+        </Button>
+      </div>
+    </Box>
   );
 }
 export default PrivateComponent;
