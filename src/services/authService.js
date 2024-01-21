b87a63c7253e34869876bafa9bbbf03ce6c0ce99
vjspranav <pranavasri@live.in>
2024-01-22 01:48:52 +0530

    Fix error handling in auth

--- src/services/authService.js
+++ src/services/authService.js
@@ -6,20 +6,23 @@ const authService = {
   isAuthenticated: true, // getAccessToken() !== null,
   // eslint-disable-next-line no-unused-vars
   login: async (email, password) => {
-    const response = await axiosInstance.post('/user/login', { email, password });
+    try {
+      const response = await axiosInstance.post('/user/login', { email, password });
 
-    if (response.status === 200) {
-      // Save tokens to local storage
-      saveTokens({
-        accessToken: response.data.access_token,
-        refreshToken: response.data.refresh_token
-      });
+      if (response.status === 200) {
+        // Save tokens to local storage
+        saveTokens({
+          accessToken: response.data.access_token,
+          refreshToken: response.data.refresh_token
+        });
 
-      authService.isAuthenticated = true;
-      return response.data;
+        authService.isAuthenticated = true;
+        return response.data;
+      }
+    } catch (err) {
+      alert('Login failed');
     }
-
-    throw new Error('Login failed');
+    return null;
   },
   logout: () => {
     clearTokens();
