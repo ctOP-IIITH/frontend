a89f87bb58bc5eca0321c3ccab01dcc9ea5c98bf
vjspranav <pranavasri@live.in>
2024-02-04 21:56:51 +0530

    Use env variable for backend api

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // eslint-disable-next-line import/prefer-default-export
 export const APP_NAME = 'ctOP';
-export const BACKEND_API_URL = 'https://xapi-ctop.vjspranav.dev';
+export const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
