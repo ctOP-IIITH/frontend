e6f61c723a76e51bca2ba0eeb05b13c5e6e199f9
vjspranav <pranavasri@live.in>
2024-04-14 22:08:18 +0530

    Add a delete button for subscriptions

--- src/components/Nodedata.js
+++ src/components/Nodedata.js
@@ -22,8 +22,9 @@ import {
   DialogContent,
   DialogActions,
   TextField,
-  Snackbar
+  Snackbar,
 } from '@mui/material';
+import { Delete as DeleteIcon } from '@mui/icons-material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import Swal from 'sweetalert2';
@@ -333,6 +334,20 @@ export default function Details() {
     </Grid>
   );
 
+  const handleDeleteSubscrpition = (index) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: `You will not be able to recover this subscription: ${subscribedUrls[index]}`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      console.log(result);
+    });
+  };
+  
+
   return loading ? (
     <CenteredLoading />
   ) : (
@@ -408,10 +423,21 @@ export default function Details() {
                     <Typography variant="h6" gutterBottom>
                       Subscribed URLs:
                     </Typography>
+
                     {subscribedUrls.map((url, index) => (
-                      <Typography key={url} variant="body1">
-                        {index + 1}. {url}
-                      </Typography>
+                      <Box
+                        key={url}
+                        display="flex"
+                        alignItems="center"
+                      justifyContent="space-between"
+                        >
+                        <Typography variant="body1">
+                          {index + 1}. {url}
+                        </Typography>
+                        <IconButton onClick={() => handleDeleteSubscrpition(index)}>
+                          <DeleteIcon />
+                        </IconButton>
+                      </Box>
                     ))}
                   </Box>
                 )}
